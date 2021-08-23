b62f35cad4ed7c4d5e43c5b77c522fb6b51e82b8
Pavlov <ipavlov@t-systems.ru>
2021-08-23 19:55:46 +0300

    creating some visual elements? some additional classes to make code more readable, starting with the pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import Counter from "./components/Counter";
 import './styles/App.css'
-import {useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPageCount} from "./utils/pages";
 
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'aa', body: 'aa'},
-        {id: 2, title: 'bb', body: 'cc'},
-        {id: 3, title: 'cc', body: 'dd'},
-        {id: 4, title: 'dd', body: 'bb'}
-
-    ])
+    const [posts, setPosts] = useState([])
     const [modal, setModal] = useState(false)
     const [filter, setFilter] = useState({sort: '', query: ''})
+    const sortedAndSearchPosts = usePosts(posts, filter.sort, filter.query)
+    const [totalPages, setTotalPages] = useState(0)
+    const  [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+
+    const [fetchPosts, isPostLoading, postError] = useFetching(async ()=>{
+        const response = await PostService.getAll(limit,page)
+        setPosts(response.data)
+        setTotalPages(getPageCount(response.headers['x-total-count'], limit))
+    })
+    useEffect(() => {
+        fetchPosts()
+    }, [filter])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -28,19 +40,6 @@ function App() {
         setPosts(posts.filter((p) => (p.id !== post.id
         )))
     }
-
-    const sortedPosts= useMemo(()=>{
-        console.log("----------Sorted function----------")
-        if (filter.sort){
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts
-    },[filter.sort, posts])
-
-    const sortedAndSearchPosts = useMemo(()=>{
-        return sortedPosts.filter((post)=> post.title.toLowerCase().includes(filter.query))
-    }, [filter.query,sortedPosts])
-
     return (
         <div className="App">
             <MyButton style={{marginTop: 30}} onClick = {() =>setModal(true)}>Create post</MyButton>
@@ -50,7 +49,12 @@ function App() {
             <Counter/>
             <hr style={{margin: '15px 0'}}/>
             <PostFilter filter={filter} setFilter={setFilter}/>
-            <Posts posts={sortedAndSearchPosts} remove={removePost} title="Some custom title"/>
+            {postError.length !==0 &&
+                <h1>Error appears {postError}</h1>}
+            {isPostLoading
+                ? <div style={{display: 'flex', justifyContent:'center', marginTop: 50}}><Loader/></div>
+                : <Posts posts={sortedAndSearchPosts} remove={removePost} title="Some custom title"/>}
+
         </div>
     );
 }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const Posts = ({posts, title, remove}) => {
-    if (posts.length ===0) {
+    if (posts.length === 0) {
         return (
             <h1 style={{textAlign: 'center'}}>There are no posts at all</h1>
         )
@@ -10,9 +11,17 @@ const Posts = ({posts, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: "center"}}>{title}</h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} index = {index+1} post = {post} key = {post.id}/>
-            )}
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} index={index + 1} post={post}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
